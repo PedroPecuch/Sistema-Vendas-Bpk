8ac45544beaa7fe181abc317558e0855660e347f
PedroPecuch <pedro.pinto@unioeste.br>
2022-01-12 14:38:18 -0300

    1.0

---
+++ Sistema_Vendas_PaoComOvo-master/src/br/com/sistema/model/Utilitarios.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistema.model;
+
+import br.com.sistema.view.Frmmenu;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Tampelini
+ */
+public class Utilitarios {
+
+    //metodo limparCampos
+    public void LimpaTela(JPanel container) {
+        Component components[] = container.getComponents();
+        for (Component component : components) {
+            if (component instanceof JTextField) {
+                ((JTextField) component).setText(null);
+            }
+        }
+    }
+
+    //Metodo para adicionar imagem de fundo JDesktopPane
+//    public void adicionaImagem() {
+//
+//        ImageIcon icon = new ImageIcon(Frmmenu.class.getResource("br.com.projeto.images/fundo.jpg"));
+//        Image img = icon.getImage();
+//
+//        JDesktopPane painel = new JDesktopPane() {
+//            public void paintComponent(Graphics g) {
+//                g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+//            }
+//
+//        }
+               
+    public boolean isAllFilled(JPanel container) {
+        boolean bool = true;
+        Component components[] = container.getComponents();
+        for (Component component : components) {
+            if (component instanceof JTextField && component.getName() == null) {
+                if (((JTextField) component).getText().isEmpty()) {
+                    bool = false;
+                    break;
+                }
+            }
+        }
+        return bool;
+    }
+    
+
+public void JavaEmail(String email, String senha) {
+    Properties props = new Properties();
+    /** Parâmetros de conexão com servidor Gmail
+    props.put("mail.smtp.host", "smtp.gmail.com");
+    props.put("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.socketFactory.class",
+    "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.port", "465");
+    */
+    
+    int stmpport = 465;
+    
+    String  d_email = "sistemavendasbiopark@gmail.com",
+        d_password = "biopark123",
+        d_host = "smtp.gmail.com",
+        d_port  = "465", //465,587
+        m_to = email,
+        m_subject = "Troca de senha",
+        m_text = "Sua nova senha é: " + senha;
+    
+    props.put("mail.smtp.user", d_email);
+    props.setProperty("mail.user", d_email);
+    props.put("mail.password", d_password);
+    props.put("mail.smtp.host", d_host);
+    props.put("mail.smtp.port", d_port);
+    props.put("mail.smtp.ssl.trust", d_host);
+    props.put("mail.smtp.ssl.enable", "true");
+    props.put("mail.smtp.debug", "true");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.starttls.required", "true");
+    props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.socketFactory.port", d_port);
+    props.put("mail.smtp.socketFactory.fallback", "false");
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.debug", true);
+
+      // Get system properties
+      System.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", d_host);
+    
+    Session session = Session.getDefaultInstance(props,
+      new javax.mail.Authenticator() {
+          @Override
+           protected PasswordAuthentication getPasswordAuthentication()
+           {
+                 return new PasswordAuthentication(d_email, d_password);
+           }
+      });
+    
+
+    //Ativa Debug para sessão 
+    session.setDebug(true);
+    
+    try {
+    
+        Message msg = new MimeMessage(session);
+        msg.setText(m_text);
+        msg.setSubject(m_subject);
+        msg.setFrom(new InternetAddress(d_email));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(m_to));
+
+        Address[] toUser = InternetAddress.parse(email);
+       
+        Transport transport = session.getTransport("smtps");
+        
+        transport.send(msg);
+
+        JOptionPane.showMessageDialog(null, "Email enviado!");
+
+     } catch (MessagingException e) {
+        e.printStackTrace();
+    }
+  }
+}
+        
+        
---
+++ Sistema_Vendas_PaoComOvo-master/src/br/com/sistema/view/FrmPedido.java
@@ -0,0 +1,438 @@
+
+package br.com.sistema.view;
+
+import br.com.sistema.dao.ProdutosDAO;
+import br.com.sistema.model.Produtos;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import org.dom4j.Document;
+
+/**
+ *
+ * @author paulo
+ */
+public class FrmPedido extends javax.swing.JFrame {
+
+    /**
+     * Creates new form FrmPedido
+     */
+    public FrmPedido() {
+        initComponents();
+    }
+    
+    public void listar() {
+
+        ProdutosDAO dao = new ProdutosDAO();
+        List<Produtos> lista = dao.listarProdutos();
+        DefaultTableModel dados = (DefaultTableModel) tbProdutosPedido.getModel();
+        dados.setNumRows(0);
+
+        for (Produtos c : lista) {
+            dados.addRow(new Object[]{
+                c.getId(),
+                c.getDescricao(),
+                c.getPreco(),
+                c.getQtd_estoque(),
+                c.getFornecedor().getNome()
+            });
+        }
+    }
+    
+    public void setarLarguraColunas() {
+        TableColumnModel modelo = tbProdutosPedido.getColumnModel();
+        
+        modelo.getColumn(0).setPreferredWidth(30);
+        modelo.getColumn(1).setPreferredWidth(300);
+        modelo.getColumn(2).setPreferredWidth(30);
+        modelo.getColumn(3).setPreferredWidth(30);
+        
+        TableColumnModel carrinho = tbCarrinhoPedido.getColumnModel();
+        
+        carrinho.getColumn(0).setPreferredWidth(10);
+        carrinho.getColumn(1).setPreferredWidth(500);
+        carrinho.getColumn(2).setPreferredWidth(20);
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbProdutosPedido = new javax.swing.JTable();
+        btnAddItem = new javax.swing.JButton();
+        jLabel16 = new javax.swing.JLabel();
+        txtpesquisa = new javax.swing.JTextField();
+        btnpesquisar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        txtQtd = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tbCarrinhoPedido = new javax.swing.JTable();
+        btnGerarPedido = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowActivated(java.awt.event.WindowEvent evt) {
+                formWindowActivated(evt);
+            }
+        });
+
+        jPanel1.setBackground(new java.awt.Color(0, 102, 204));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Gerar Pedido");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addContainerGap(639, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        tbProdutosPedido.setAutoCreateRowSorter(true);
+        tbProdutosPedido.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        tbProdutosPedido.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Código", "Descrição", "Preço", "Qtd. Estoque"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Float.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        tbProdutosPedido.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tbProdutosPedidoMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tbProdutosPedido);
+
+        btnAddItem.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnAddItem.setText("Adicionar Item");
+        btnAddItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddItemActionPerformed(evt);
+            }
+        });
+
+        jLabel16.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel16.setText("Nome:");
+
+        txtpesquisa.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txtpesquisa.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtpesquisaKeyPressed(evt);
+            }
+        });
+
+        btnpesquisar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnpesquisar.setText("Pesquisar");
+        btnpesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnpesquisarActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Quantidade:");
+
+        txtQtd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtQtdActionPerformed(evt);
+            }
+        });
+
+        jScrollPane2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        tbCarrinhoPedido.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Código", "Produto", "Quantidade"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(tbCarrinhoPedido);
+
+        btnGerarPedido.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnGerarPedido.setText("Gerar Pedido");
+        btnGerarPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGerarPedidoActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel16)
+                                .addGap(9, 9, 9)
+                                .addComponent(txtpesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnpesquisar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtQtd, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnAddItem)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btnGerarPedido)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(5, 5, 5)
+                        .addComponent(jLabel16))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(txtpesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnpesquisar)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAddItem)
+                    .addComponent(jLabel2)
+                    .addComponent(txtQtd, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnGerarPedido)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+        
+    private void tbProdutosPedidoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbProdutosPedidoMouseClicked
+        
+        
+    }//GEN-LAST:event_tbProdutosPedidoMouseClicked
+
+    private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
+        // TODO add your handling code here:
+        listar();
+        setarLarguraColunas();
+    }//GEN-LAST:event_formWindowActivated
+
+    private void txtpesquisaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtpesquisaKeyPressed
+        String nome = "%" + txtpesquisa.getText() + "%";
+
+        ProdutosDAO dao = new ProdutosDAO();
+        List<Produtos> lista = dao.listarProdutosPorNome(nome);
+
+        DefaultTableModel dados = (DefaultTableModel) tbProdutosPedido.getModel();
+        dados.setNumRows(0);
+
+        for (Produtos c : lista) {
+            dados.addRow(new Object[]{
+                c.getId(),
+                c.getDescricao(),
+                c.getPreco(),
+                c.getQtd_estoque(),
+                c.getFornecedor().getNome()
+            });
+
+        }
+    }//GEN-LAST:event_txtpesquisaKeyPressed
+
+    private void btnpesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnpesquisarActionPerformed
+        // Botao pesquisar
+        String nome = "%" + txtpesquisa.getText() + "%";
+
+        ProdutosDAO dao = new ProdutosDAO();
+        List<Produtos> lista = dao.listarProdutosPorNome(nome);
+
+        DefaultTableModel dados = (DefaultTableModel) tbProdutosPedido.getModel();
+        dados.setNumRows(0);
+
+        for (Produtos c : lista) {
+            dados.addRow(new Object[]{
+                c.getId(),
+                c.getDescricao(),
+                c.getPreco(),
+                c.getQtd_estoque(),
+                c.getFornecedor().getNome()
+            });
+
+        }
+
+    }//GEN-LAST:event_btnpesquisarActionPerformed
+
+    private void txtQtdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtQtdActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtQtdActionPerformed
+
+    private void btnAddItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddItemActionPerformed
+        // Adiciona um item ao carrinho.
+        if(!txtQtd.getText().isEmpty()) {
+        int qtd = Integer.parseInt(txtQtd.getText());
+        int codigo = Integer.parseInt(tbProdutosPedido.getValueAt(tbProdutosPedido.getSelectedRow(), 0).toString());
+        String produto = tbProdutosPedido.getValueAt(tbProdutosPedido.getSelectedRow(), 1).toString();
+        
+        DefaultTableModel pedido = (DefaultTableModel) tbCarrinhoPedido.getModel();
+        
+        pedido.addRow(new Object[]{
+            codigo,
+            produto,
+            qtd
+        });
+    } else {
+            JOptionPane.showMessageDialog(null, "Informe uma quantidade.");
+    }
+    }//GEN-LAST:event_btnAddItemActionPerformed
+
+    private void btnGerarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGerarPedidoActionPerformed
+        //Gera um pdf com os dados da tabela.
+        
+        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+        
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("Pedido.pdf"));
+            
+            document.open();
+            
+            document.add(new Paragraph("PEDIDO DE COMPRA"));
+            document.add(new Paragraph(""));
+            
+            PdfPTable tabela = new PdfPTable(3);
+            
+            //Adicionando os headers.
+            tabela.addCell("Código");
+            tabela.addCell("Produto");
+            tabela.addCell("Quant.");
+            
+            for (int i = 0; i < tbCarrinhoPedido.getRowCount(); i++) {
+                String id = tbCarrinhoPedido.getValueAt(i, 0).toString();
+                String produto = tbCarrinhoPedido.getValueAt(i, 1).toString();
+                String quant = tbCarrinhoPedido.getValueAt(i, 2).toString();
+                
+                tabela.addCell(id);
+                tabela.addCell(produto);
+                tabela.addCell(quant);
+            }
+            
+            document.add(tabela);
+            
+        } catch (FileNotFoundException | DocumentException ex) {
+            JOptionPane.showMessageDialog(null, "Erro: " +ex);
+        } finally {
+            document.close();
+        }
+        
+        try {
+            Desktop.getDesktop().open(new File("Pedido.pdf"));
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Erro: " +ex);
+        }
+    }//GEN-LAST:event_btnGerarPedidoActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmPedido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrmPedido().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddItem;
+    private javax.swing.JButton btnGerarPedido;
+    private javax.swing.JButton btnpesquisar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable tbCarrinhoPedido;
+    private javax.swing.JTable tbProdutosPedido;
+    private javax.swing.JTextField txtQtd;
+    private javax.swing.JTextField txtpesquisa;
+    // End of variables declaration//GEN-END:variables
+}
